6d606e56ca0f3c4c1e9882d62d7d8c736880cd0f
Artyom Lebedev <arlebedev@ugcs.com>
2020-05-26 18:27:39 +0300

    Video streamer docs update.

--- video-streamer/search/all_e.js
+++ video-streamer/search/all_e.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['tailnumber',['tailNumber',['../structVsParams.html#a3843b8291183b17e4fdf27532d48b628',1,'VsParams::tailNumber()'],['../structStreamer_1_1Params.html#a7cd8c10d4984ceb056fe8797fc675c1d',1,'Streamer::Params::tailNumber()']]],
-  ['targeturi',['targetUri',['../structVsParams.html#a5a15467b14b7d0f92d0991766f557fc6',1,'VsParams']]],
+  ['targeturi',['targetUri',['../structVsParams.html#a5a15467b14b7d0f92d0991766f557fc6',1,'VsParams::targetUri()'],['../structStreamer_1_1Params.html#a3aa59b08e0199447faa347172b779ba4',1,'Streamer::Params::targetUri()']]],
   ['transcodebitrate',['transcodeBitrate',['../structVsParams.html#a51468754a19915566ed02a34ea043bb4',1,'VsParams::transcodeBitrate()'],['../structStreamer_1_1Params.html#a53296e88b0c7be35b49a33ebd9bf1596',1,'Streamer::Params::transcodeBitrate()']]],
   ['transcodeframeratelimit',['transcodeFramerateLimit',['../structVsParams.html#a357c5be54a742e2046ec94d094e473e3',1,'VsParams::transcodeFramerateLimit()'],['../structStreamer_1_1Params.html#a031dd52666661dea122ca221c1b37baf',1,'Streamer::Params::transcodeFramerateLimit()']]],
   ['transcodescale',['transcodeScale',['../structVsParams.html#a1a768dc146a7b2d5fb360cf26dc677df',1,'VsParams::transcodeScale()'],['../structStreamer_1_1Params.html#a4f8e100d2c91813146090d5f08ce2c1c',1,'Streamer::Params::transcodeScale()']]]
